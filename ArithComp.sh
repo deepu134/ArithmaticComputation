26416a310d0366465b07980e952774b8b6482d86
deepak r kulal <deepakrkulal123@gmail.com>
2020-10-09 15:14:07 +0100

    Added:Expretion 3

--- ArithComp.sh
+++ ArithComp.sh
@@ -3,4 +3,5 @@ echo "SORTING ARITHMATIC COMPUTATION PROBLEM"
 read -p "ENTER THREE NUMBERS SEPARATED BY SPACE" A B C
 exp1=$(($A+$B*$C))
 exp2=$(($A*$B+$C))
-echo $exp2
+exp3=`echo "$A $B $C" |awk '{print $1/$2+$3}'`
+echo $exp3
